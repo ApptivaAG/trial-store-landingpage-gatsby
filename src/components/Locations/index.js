3c539118244a3ffafe9ef379cd491939aaed028c
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2018-12-04 12:02:17 +0100

    iframe props to react

--- src/components/Locations/index.js
+++ src/components/Locations/index.js
@@ -84,13 +84,13 @@ class Locations extends React.Component {
                 <iframe
                   title="Google Maps"
                   scrolling="no"
-                  marginheight="0"
-                  marginwidth="0"
+                  marginHeight="0"
+                  marginWidth="0"
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2701.8487454157225!2d8.53579145140853!3d47.37587051177883!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47900a061193d549%3A0x753857550252252f!2sGLOBUS+Z%C3%BCrich+Warenhaus!5e0!3m2!1sen!2sch!4v1537104489336"
                   data-reactid="358"
                   width="100%"
                   height="550px"
-                  frameborder="0"
+                  frameBorder="0"
                 />
               </div>
             </div>
