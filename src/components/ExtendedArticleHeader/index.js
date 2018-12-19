baae0806fbf25d02b77851654468f724acfc45f8
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2018-12-19 13:02:43 +0100

    Removed snowflakes and dezember

--- src/components/ExtendedArticleHeader/index.js
+++ src/components/ExtendedArticleHeader/index.js
@@ -2,37 +2,11 @@ import { Link } from 'gatsby'
 import React from 'react'
 import './style.scss'
 
-const snowflake = (leftOrRight = 'left', key) => (
-  <img
-    style={{
-      top: `${Math.random() * 20}vw`,
-      [leftOrRight]: `${2 + Math.random() * 20}vw`,
-      height: `${Math.random() * 6}vw`,
-      transform: `rotate(${Math.random() * 50}deg)`,
-    }}
-    src="/img/snowflake.svg"
-    alt=""
-    key={key}
-  />
-)
-
-const snowflakes = () => {
-  var count
-  var flakes = []
-  for (count = 0; count < 8; count++) {
-    flakes.push(snowflake('left', count))
-  }
-  for (count = 0; count < 8; count++) {
-    flakes.push(snowflake('right', count + 8))
-  }
-  return flakes
-}
-
 const ExtendedArticleHeader = () => (
   <section id="articles" className="article-heading">
     <div className="container">
       <div className="text-center">
-        <h2 className="section-heading text-uppercase">HIGHLIGHTS IM DEZEMBER</h2>
+        <h2 className="section-heading text-uppercase">HIGHLIGHTS IM JANUAR</h2>
         <blockquote>Leih’ dir aus, was dich gl&uuml;cklich macht.</blockquote>
         <p className="section-heading">
           Überrasch’ dich selbst! Im Ausleih-Store f&uuml;r Luxus hast du alle Freiheiten. Leih’ dir unsere
@@ -44,7 +18,6 @@ const ExtendedArticleHeader = () => (
       </div>
       <p className="info-text">Nur Online und in allen teilnehmenden Filialen erh&auml;ltlich!</p>
     </div>
-    <div className="snowflakes">{snowflakes()}</div>
   </section>
 )
 
