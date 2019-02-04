bd0e8e3bedf7ecd97d97e717795aac8f07f54a7a
Leandrawuethrich <46598963+Leandrawuethrich@users.noreply.github.com>
2019-02-04 08:55:18 +0100

    Update index.js
    Aktualisierung Einleitungstext

--- src/components/ExtendedArticleHeader/index.js
+++ src/components/ExtendedArticleHeader/index.js
@@ -6,14 +6,14 @@ const ExtendedArticleHeader = () => (
   <section id="articles" className="article-heading">
     <div className="container">
       <div className="text-center">
-        <h2 className="section-heading text-uppercase">HIGHLIGHTS IM JANUAR</h2>
+        <h2 className="section-heading text-uppercase">HIGHLIGHTS IM FEBRUAR</h2>
         <blockquote>Leih’ dir aus, was dich gl&uuml;cklich macht.</blockquote>
         <p className="section-heading">
           Überrasch’ dich selbst! Im Ausleih-Store f&uuml;r Luxus hast du alle Freiheiten. Leih’ dir unsere
-          Dezember-Highlights so lange du willst. Die neueste Mode, Accessoires und Gadgets der angesagtesten Brands
-          wurden exklusiv von GLOBUS-Experten f&uuml;r dich kuratiert – f&uuml;r die festlichste Zeit des Jahres.
+          Februar-Highlights so lange du willst. Die neueste Mode, Accessoires und Gadgets der angesagtesten Brands
+          wurden exklusiv von GLOBUS-Experten f&uuml;r dich kuratiert – f&uuml;r freu dich auf den Frühling.
           <br />
-          Viel Spass beim Feiern!
+          Viel Spass beim Stöbern!
         </p>
       </div>
       <p className="info-text">Nur Online und in allen teilnehmenden Filialen erh&auml;ltlich!</p>
