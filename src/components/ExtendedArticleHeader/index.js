89e4484cdbfb584aa6d1457c53775a53fb2b6eec
Leandrawuethrich <46598963+Leandrawuethrich@users.noreply.github.com>
2019-03-15 08:01:26 +0100

    Update index.js

--- src/components/ExtendedArticleHeader/index.js
+++ src/components/ExtendedArticleHeader/index.js
@@ -6,11 +6,11 @@ const ExtendedArticleHeader = () => (
   <section id="articles" className="article-heading">
     <div className="container">
       <div className="text-center">
-        <h2 className="section-heading text-uppercase">HIGHLIGHTS IM M&Auml;</h2>
+        <h2 className="section-heading text-uppercase">HIGHLIGHTS IM M&auml;rz</h2>
         <blockquote>Leih’ dir aus, was dich gl&uuml;cklich macht.</blockquote>
         <p className="section-heading">
           Überrasch’ dich selbst! Im Ausleih-Store f&uuml;r Luxus hast du alle Freiheiten. Leih’ dir unsere
-          M&Auml;-Highlights so lange du willst. Die neueste Mode, Accessoires und Gadgets der angesagtesten Brands
+          M&auml;rz-Highlights so lange du willst. Die neueste Mode, Accessoires und Gadgets der angesagtesten Brands
           wurden exklusiv von GLOBUS-Experten f&uuml;r dich kuratiert – freu dich auf den Fr&uuml;hling.
           <br />
           Viel Spass beim St&ouml;bern!
