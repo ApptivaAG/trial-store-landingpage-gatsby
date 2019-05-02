7db6b6164bbb2e861d0202ef9a6f28697d7b43a5
Leandrawuethrich <46598963+Leandrawuethrich@users.noreply.github.com>
2019-05-02 08:16:33 +0200

    Anpassung Monat

--- src/components/ExtendedArticleHeader/index.js
+++ src/components/ExtendedArticleHeader/index.js
@@ -6,11 +6,11 @@ const ExtendedArticleHeader = () => (
   <section id="articles" className="article-heading">
     <div className="container">
       <div className="text-center">
-        <h2 className="section-heading text-uppercase">HIGHLIGHTS IM APRIL</h2>
+        <h2 className="section-heading text-uppercase">HIGHLIGHTS IM MAI</h2>
         <blockquote>Leih’ dir aus, was dich gl&uuml;cklich macht.</blockquote>
         <p className="section-heading">
           Überrasch’ dich selbst! Im Ausleih-Store f&uuml;r Luxus hast du alle Freiheiten. Leih’ dir unsere
-          April-Highlights so lange du willst. Die neueste Mode, Accessoires und Gadgets der angesagtesten Brands
+          Mai-Highlights so lange du willst. Die neueste Mode, Accessoires und Gadgets der angesagtesten Brands
           wurden exklusiv von GLOBUS-Experten f&uuml;r dich kuratiert.
           <br />
           Viel Spass beim St&ouml;bern!
